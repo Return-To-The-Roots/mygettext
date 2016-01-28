d780f4cb6ee7208e4f65c22067b2030f49921130
Alexander Grund <Alex@grundis.de>
2016-01-29 00:54:52 +0100

    Add DebugNew.h

--- src/gettext.cpp
+++ src/gettext.cpp
@@ -30,13 +30,8 @@
 
 namespace bfs = boost::filesystem;
 
-///////////////////////////////////////////////////////////////////////////////
-// Makros / Defines
-#if defined _WIN32 && defined _DEBUG && defined _MSC_VER && defined _MSC_VER
-#define new new(_NORMAL_BLOCK, THIS_FILE, __LINE__)
-#undef THIS_FILE
-static char THIS_FILE[] = __FILE__;
-#endif
+// Include last!
+#include "DebugNew.h"
 
 GetText::GetText() : catalog_("messages"), directory_("/usr/share/locale"), locale_("C"), isLoaded(false), iconv_cd_(0)
 {
